dcd0e18800333430b32c866cd8dd7b8b0107ca92
AtomicPages LLC <contact@atomicpages.net>
2013-10-02 08:20:08 -0700

    0.3.19
    Fixed slider error

--- assets/functions.helper.php
+++ assets/functions.helper.php
@@ -23,16 +23,23 @@ if(!function_exists("skeleton_header_image_helper_width")) {
 if(!function_exists("skeleton_slider")) {
 	function skeleton_slider() {
 		global $data;
-		
-		$slider = '<div class="home-slider">';
-		$slider .= '<ul class="slides">';
-		$slides = $data['pingu_slider'];
-		foreach($slides as $slide) {
-			$slider .= '<li><img src="' . $slide['url'] . '"></li>';
+		if(is_array($data['pingu_slider'])) {
+			$slider .= '<div class="wrapper slider">';
+			$slider .= '<section class="container sliderc">';
+			$slider  = '<div class="sixteen columns home-slider">';
+			$slider .= '<ul class="slides">';
+			$slides  = $data['pingu_slider'];
+			foreach($slides as $slide) {
+				$slider .= '<li><img src="' . $slide['url'] . '"></li>';
+			}
+			$slider .= "</ul>";
+			$slider .= "</div>";
+			$slider .= "</section>";
+			$slider .= "</div>";
+
+			echo $slider;
 		}
-		$slider .= "</ul>";
-		$slider .= "</div>";
-		
-		echo $slider;
+
+		return false;
 	}
 }
